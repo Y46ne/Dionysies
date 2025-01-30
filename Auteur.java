fb689048b455a00c4c7fbc0c5d94745b618decc3
Y46ne <yassine.belaarous@univ-orleans.fr>
2025-01-30 08:38:16 +0100

    Ajout du constructeur dans Auteur.java et des getteurs

--- Auteur.java
+++ Auteur.java
@@ -1,4 +1,6 @@
 class Auteur {
+  String nom;
+
   String citation_tragédie;
   int qualité_tragédie;
 
@@ -16,8 +18,56 @@ class Auteur {
       String citation_comédie,
       int qualité_drame,
       String citation_drame) {
-    //TODO
+    this.nom = nom;
+    this.citation_tragédie = citation_tragédie;
+    this.qualité_tragédie = qualité_tragédie;
+    this.citation_comédie = citation_comédie;
+    this.qualité_comédie = qualité_comédie;
+    this.citation_drame = citation_drame;
+    this.citation_drame = citation_drame;
   }
+
+
+  public String getNom() {
+    return nom;
+  }
+
+
+  public String getCitation_tragédie() {
+    return citation_tragédie;
+  }
+
+
+
+  public int getQualité_tragédie() {
+    return qualité_tragédie;
+  }
+
+
+
+  public String getCitation_comédie() {
+    return citation_comédie;
+  }
+
+
+
+  public int getQualité_comédie() {
+    return qualité_comédie;
+  }
+
+
+
+  public String getCitation_drame() {
+    return citation_drame;
+  }
+
+
+
+  public int getQualité_drame() {
+    return qualité_drame;
+  }
+
+  
 
   public Style point_fort() {
     if (qualité_tragédie > qualité_comédie) {
