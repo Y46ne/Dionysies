da4df01d59f274c442a43f7291563ab9b7c09949
Y46ne <yassine.belaarous@univ-orleans.fr>
2025-01-30 10:47:12 +0100

    Ajout de test et des truc je sais meme pas si sa marche mais az on est la

--- Epreuve.java
+++ Epreuve.java
@@ -1,6 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Epreuve {
     int nombreSpectateurs;
     Style style;
+    List<Auteur> auteurs;
+    List<Integer> score=new ArrayList<Integer>();
 
     public Epreuve(int nombreSpectateurs, Style style) {
         this.nombreSpectateurs = nombreSpectateurs;
@@ -23,10 +28,29 @@ public class Epreuve {
         this.style = style;
     }
 
+
     public int getScore(Auteur auteur) {
         return auteur.QualitéStyle(style) * this.nombreSpectateurs;
     }
 
+    public void addAuteur(Auteur auteur) {
+        auteurs.add(auteur);
+    }
+
+    public void removeAuteur(Auteur auteur) {
+        auteurs.remove(auteur);
+    }
+
+    public void addScore() {
+        for (Auteur auteur : auteurs) {
+            int total = 0;
+            total += getScore(auteur);
+            score.add(total);
+        }
+    }
+
+
+    @Override
     public String toString() {
         return "Epreuve{" +
                 "nombreSpectateurs=" + nombreSpectateurs +
